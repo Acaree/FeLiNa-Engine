84132c780176394402acbb3b9042c0d3e5e1af58
Siitoo <alfonsosanchez1896@gmail.com>
2018-09-18 10:29:09 +0200

    Cleaning includes in ModuleSceneIntro .h/.cpp

--- FeLiNa/ModuleSceneIntro.cpp
+++ FeLiNa/ModuleSceneIntro.cpp
@@ -1,15 +1,14 @@
-#include "Globals.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
-#include "Primitive.h"
-#include "PhysBody3D.h"
+
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
 
 ModuleSceneIntro::~ModuleSceneIntro()
-{}
+{
+}
 
 // Load assets
 bool ModuleSceneIntro::Start()
--- FeLiNa/ModuleSceneIntro.h
+++ FeLiNa/ModuleSceneIntro.h
@@ -1,13 +1,8 @@
 #pragma once
+
 #include "Module.h"
-#include "p2DynArray.h"
-#include "Globals.h"
 #include "Primitive.h"
 
-#define MAX_SNAKE 2
-
-struct PhysBody3D;
-struct PhysMotor3D;
 
 class ModuleSceneIntro : public Module
 {
