98ac3d2705c18fb82e73ac99babd27f0ca271e64
Acaree <acampamar@gmail.com>
2018-10-02 20:45:36 +0200

    Cube arrays and index full added
    Also it's a cube, not only a triangle

--- FeLiNa/Cube_ArraysandIndex.cpp
+++ FeLiNa/Cube_ArraysandIndex.cpp
@@ -1,30 +1,21 @@
-#include "Cube_ArraysandIndices.h"
+#include "Cube_ArraysandIndex.h"
 
-Cube_ArraysandIndices::Cube_ArraysandIndices(float3 position, int size, GeometryType type) : Geometry(position, size, type) {
+Cube_ArraysandIndex::Cube_ArraysandIndex(float3 position, int size, GeometryType type) : Geometry(position, size, type) {
 
-	vertexs[0] = 1.0f;
-	vertexs[1] = 1.0f;
-	vertexs[2] = 0.0f;
-	vertexs[3] = 0.0f;
-	vertexs[4] = 1.0f;
-	vertexs[5] = 0.0f;
-	vertexs[6] = 1.0f;
-	vertexs[7] = 1.0f;
-	vertexs[8] = 1.0f;
 	
 	glGenBuffers(1, (GLuint*) &(my_id));
 	glBindBuffer(GL_ARRAY_BUFFER, my_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 36 * 3, vertexs, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8 * 3, vertexs, GL_STATIC_DRAW);
 	
 
 
 	glGenBuffers(1, (GLuint*) &(my_indices));
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 8, indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * 36, indices, GL_STATIC_DRAW);
 
 }
 
-void Cube_ArraysandIndices::Render() {
+void Cube_ArraysandIndex::Render() {
 
 	
 
@@ -34,7 +25,7 @@ void Cube_ArraysandIndices::Render() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, my_indices);
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, NULL);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glDisableClientState(GL_VERTEX_ARRAY);
 
