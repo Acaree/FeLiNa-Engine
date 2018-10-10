76e27db4da78737f5a10f29674921931a568509e
Siitoo <alfonsosanchez1896@gmail.com>
2018-10-10 18:45:33 +0200

    Screenshot with devil and erase freeimage library

--- FeLiNa/ModuleImage.cpp
+++ FeLiNa/ModuleImage.cpp
@@ -2,7 +2,10 @@
 #include "Application.h"
 #include "ModuleWindow.h"
 #include "Gif/gif.h"
-#include "imgui-1.65/imgui.h"
+
+#include "Devil\include\ilut.h"
+
+
 ModuleImage::ModuleImage(int widht, int height) : width(widht), height(height)
 {
 	pixels = new BYTE[3 * widht*height];
@@ -18,19 +21,21 @@ ModuleImage::~ModuleImage()
 
 bool ModuleImage::TakeScreenshoot()
 {
-	glReadPixels(0, 0, width, height, GL_BGR, GL_UNSIGNED_BYTE, pixels);
-	FIBITMAP* image = FreeImage_ConvertFromRawBits(pixels,width,height,3*width,24, 0x0000FF, 0xFF0000, 0x00FF00, false);
+	ILuint imageID = ilGenImage();
+	ilBindImage(imageID);
+	ilutGLScreen();
+	ilEnable(IL_FILE_OVERWRITE);
 
 	time_t actual_time = time(0);
 	tm reported_time;
 	localtime_s(&reported_time, &actual_time);
 
-	static char tmp[30];
-	sprintf_s(tmp, 1024, "Screenshots_gif/Screenshot_%i_%i_%i.%i.%i.png", reported_time.tm_mday,reported_time.tm_mon,reported_time.tm_hour, reported_time.tm_min, reported_time.tm_sec);
+	static char tmp[50];
+	sprintf_s(tmp, 1024, "Screenshots_gif/Screenshot_%i_%i_%i.%i.%i.gif", reported_time.tm_mday, reported_time.tm_mon, reported_time.tm_hour, reported_time.tm_min, reported_time.tm_sec);
 
-	bool x = FreeImage_Save(FIF_PNG, image, tmp);
-
-	FreeImage_Unload(image);
+	ilSave(IL_PNG, tmp);
+	ilDisable(IL_FILE_OVERWRITE);
+	ilDeleteImage(imageID);
 	
 	return true;
 }
@@ -51,7 +56,7 @@ void ModuleImage::TakeScreenGif(float dt)
 			tm reported_time;
 			localtime_s(&reported_time, &actual_time);
 
-			static char tmp[30];
+			static char tmp[50];
 			sprintf_s(tmp, 1024, "Screenshots_gif/Gif_%i_%i_%i.%i.%i.gif", reported_time.tm_mday, reported_time.tm_mon, reported_time.tm_hour, reported_time.tm_min, reported_time.tm_sec);
 
 
