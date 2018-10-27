7014a336a0b6d3829493deefd2de2d7f1a43e9b1
Acaree <acampamar@gmail.com>
2018-10-27 21:04:55 +0200

    Inspector transform have functional rotation and scale

--- FeLiNa/ComponentTransform.cpp
+++ FeLiNa/ComponentTransform.cpp
@@ -38,8 +38,6 @@ void ComponentTransform::SetRotation(float3 new_rotation) {
 	
 	euler_angles = new_rotation;
 
-	quat_rotation = math::Quat::FromEulerXYZ(euler_angles.x * DEGTORAD, euler_angles.y * DEGTORAD, euler_angles.z * DEGTORAD);
-
 	UpdateMatrix();
 }
 
@@ -65,6 +63,8 @@ float3 ComponentTransform::GetScale()const{
 
 void ComponentTransform :: UpdateMatrix() {
 
+	quat_rotation = math::Quat::FromEulerXYZ(euler_angles.x * DEGTORAD, euler_angles.y * DEGTORAD, euler_angles.z * DEGTORAD);
+
 	transform_matrix = float4x4::FromTRS(position,quat_rotation, scale);
 }
 
@@ -81,55 +81,152 @@ Quat ComponentTransform::GetQuaternion() const
 void ComponentTransform::DrawInspector()
 {
 
-	ImGui::Text("Position:");
+	if (ImGui::CollapsingHeader("Transform")) {
 
-	char Pos_x_c[16] = {};
+		if (ImGui::CollapsingHeader("Position:")) {
 
-	sprintf_s(Pos_x_c, "%.2f", position.x);
-	
-	if (ImGui::InputText("x", Pos_x_c, sizeof(Pos_x_c))) {
 
-		float temp = atof(Pos_x_c);
+			char Pos_x_c[16] = {};
 
-		position.x = temp;
+			sprintf_s(Pos_x_c, "%.2f", position.x);
 
-		UpdateMatrix();
+			if (ImGui::InputText("x", Pos_x_c, sizeof(Pos_x_c))) {
+
+				float temp = atof(Pos_x_c);
+
+				position.x = temp;
+
+				UpdateMatrix();
+			}
+
+
+
+			char Pos_y_c[16] = {};
+
+			sprintf_s(Pos_y_c, "%.2f", position.y);
+
+			if (ImGui::InputText("y", Pos_y_c, sizeof(Pos_y_c))) {
+
+				float temp = atof(Pos_y_c);
+
+				position.y = temp;
+
+				UpdateMatrix();
+			}
+
+
+			char Pos_z_c[16] = {};
+
+			sprintf_s(Pos_z_c, "%.2f", position.z);
+
+			if (ImGui::InputText("z", Pos_z_c, sizeof(Pos_z_c))) {
+
+				float temp = atof(Pos_z_c);
+
+				position.z = temp;
+
+				UpdateMatrix();
+			}
+
+		}
+
+		if (ImGui::CollapsingHeader("Rotation:")) {
+
+
+			char Rot_x_c[16] = {};
+
+			sprintf_s(Rot_x_c, "%.2f", euler_angles.x);
+
+			if (ImGui::InputText("x", Rot_x_c, sizeof(Rot_x_c))) {
+
+				float temp = atof(Rot_x_c);
+
+				euler_angles.x = temp;
+
+				UpdateMatrix();
+			}
+
+
+
+			char Rot_y_c[16] = {};
+
+			sprintf_s(Rot_y_c, "%.2f", euler_angles.y);
+
+			if (ImGui::InputText("y", Rot_y_c, sizeof(Rot_y_c))) {
+
+				float temp = atof(Rot_y_c);
+
+				euler_angles.y = temp;
+
+				UpdateMatrix();
+			}
+
+
+			char Rot_z_c[16] = {};
+
+			sprintf_s(Rot_z_c, "%.2f", euler_angles.z);
+
+			if (ImGui::InputText("z", Rot_z_c, sizeof(Rot_z_c))) {
+
+				float temp = atof(Rot_z_c);
+
+				euler_angles.z = temp;
+
+				UpdateMatrix();
+			}
+
+		}
+
+		if (ImGui::CollapsingHeader("Scale:")) {
+
+
+			char Sc_x_c[16] = {};
+
+			sprintf_s(Sc_x_c, "%.2f", scale.x);
+
+			if (ImGui::InputText("x", Sc_x_c, sizeof(Sc_x_c))) {
+
+				float temp = atof(Sc_x_c);
+				scale.x = temp;
+
+				UpdateMatrix();
+			}
+
+
+
+			char Sc_y_c[16] = {};
+
+			sprintf_s(Sc_y_c, "%.2f", scale.y);
+
+			if (ImGui::InputText("y", Sc_y_c, sizeof(Sc_y_c))) {
+
+				float temp = atof(Sc_y_c);
+
+				scale.y = temp;
+
+				UpdateMatrix();
+			}
+
+
+			char Sc_z_c[16] = {};
+
+			sprintf_s(Sc_z_c, "%.2f", scale.z);
+
+			if (ImGui::InputText("z", Sc_z_c, sizeof(Sc_z_c))) {
+
+				float temp = atof(Sc_z_c);
+
+				scale.z = temp;
+
+				UpdateMatrix();
+			}
+
+		}
+
 	}
-
-
-
-	char Pos_y_c[16] = {};
-
-	sprintf_s(Pos_y_c, "%.2f", position.y);
-
-	if (ImGui::InputText("y", Pos_y_c, sizeof(Pos_y_c))) {
-
-		float temp = atof(Pos_y_c);
-
-		position.y = temp;
-
-		UpdateMatrix();
-	}
-
-
-	char Pos_z_c[16] = {};
-
-	sprintf_s(Pos_z_c, "%.2f", position.z);
-
-	if (ImGui::InputText("z", Pos_z_c, sizeof(Pos_z_c))) {
-
-		float temp = atof(Pos_z_c);
-
-		position.z = temp;
-
-		UpdateMatrix();
-	}
-
-
 	
-	ImGui::Text("Rotation: %f %f %f", euler_angles.x, euler_angles.y, euler_angles.z);
-
 	
-	ImGui::Text("Scale: %f %f %f", scale.x, scale.y, scale.z);
+	
+	
 
 }
