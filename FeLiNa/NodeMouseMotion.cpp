15e9efe9eb00356b89a84202b4f2527eb4cd7de0
Siitoo <alfonsosanchez1896@gmail.com>
2018-12-04 09:34:27 +0100

    Node mouse motion

---
+++ FeLiNa/NodeMouseMotion.cpp
@@ -0,0 +1,7 @@
+#include "NodeMouseMotion.h"
+
+
+NodeMouseMotion::NodeMouseMotion(): Node(9, "Mouse Motion", { 100,100 }, 0, 1, NodeType::FunctionType)
+{
+
+}
---
+++ FeLiNa/NodeMouseMotion.h
@@ -0,0 +1,16 @@
+#ifndef _NODE_MOUSE_MOTION_
+#define _NODE_MOUSE_MOTION_
+
+#include "NodeGraph.h"
+
+
+class NodeMouseMotion : public Node
+{
+public:
+
+	NodeMouseMotion();
+
+
+};
+
+#endif
